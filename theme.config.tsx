f059cb1cde661fd0ec4215f03571ab6f2f80edee
earth2travis <travis.mccutcheon@gmail.com>
2023-07-19 15:27:01 -0500

    Update theme configuration

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,18 +1,18 @@
-import React from 'react'
-import { DocsThemeConfig } from 'nextra-theme-docs'
+import React from 'react';
+import { DocsThemeConfig } from 'nextra-theme-docs';
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>DAOhaus User Guide</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/HausDAO/',
   },
   chat: {
-    link: 'https://discord.com',
+    link: 'https://discord.gg/daohaus',
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: 'https://github.com/HausDAO/',
   footer: {
-    text: 'Nextra Docs Template',
+    text: 'DAOhaus User Guide',
   },
-}
+};
 
-export default config
+export default config;
