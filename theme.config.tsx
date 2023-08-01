6f7641b79181110f5ec64bbaedf2777383437fcb
lvpiqi <96166538+lvpiqi@users.noreply.github.com>
2023-08-01 11:23:00 +0800

    Update theme.config.tsx

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>dhffffffdddMy Project</span>,
   project: {
     link: 'https://github.com/shuding/nextra-docs-template',
   },
